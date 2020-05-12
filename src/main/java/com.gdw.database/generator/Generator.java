f9f86c4d57aa177c87678b26fedd28aa4987cad9
Wenhao Guo <35193002+kobeandonear@users.noreply.github.com>
2020-05-12 10:14:28 +0800

    Update Generator.java
    泛型修改，返回值泛型

--- src/main/java/com.gdw.database/generator/Generator.java
+++ src/main/java/com.gdw.database/generator/Generator.java
@@ -18,7 +18,7 @@ import java.util.List;
  *
  * @author guowenhao6
  */
-public class Generator<T> {
+public class Generator {
     private static final List<String> types = Arrays.asList("java.lang.Integer",
             "java.lang.Double",
             "java.lang.Float",
@@ -32,52 +32,57 @@ public class Generator<T> {
     private String suffix = "";
 
 
-    public T generate(Object obj,Class<T> CriteriaClass) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        CriteriaInfo criteriaInfoAnnotation = obj.getClass().getAnnotation(CriteriaInfo.class);
-        if(criteriaInfoAnnotation!=null){
-            createCriteriaMethodName = criteriaInfoAnnotation.criteriaInitMethodName();
-            prefix = criteriaInfoAnnotation.fieldPrefix();
-            suffix = criteriaInfoAnnotation.fieldSuffix();
-        }
-        T instance = CriteriaClass.newInstance();
-        Method createCriteria = CriteriaClass.getDeclaredMethod(createCriteriaMethodName);
-        createCriteria.setAccessible(true);
-        Object criteria = createCriteria.invoke(instance);
+    public<T> T generate(Object obj,Class<T> CriteriaClass) {
+        try {
+            CriteriaInfo criteriaInfoAnnotation = obj.getClass().getAnnotation(CriteriaInfo.class);
+            if (criteriaInfoAnnotation != null) {
+                createCriteriaMethodName = criteriaInfoAnnotation.criteriaInitMethodName();
+                prefix = criteriaInfoAnnotation.fieldPrefix();
+                suffix = criteriaInfoAnnotation.fieldSuffix();
+            }
+            T instance = CriteriaClass.newInstance();
+            Method createCriteria = CriteriaClass.getDeclaredMethod(createCriteriaMethodName);
+            createCriteria.setAccessible(true);
+            Object criteria = createCriteria.invoke(instance);
 
-        Field[] fields = obj.getClass().getDeclaredFields();
-        for (Field field : fields){
-            field.setAccessible(true);
-            Object value = field.get(obj);
-            if(value==null){
-                continue;
-            }
-            if(!types.contains(value.getClass().getName())&&!(value instanceof List)&&!(value instanceof Enum)){
-                continue;
-            }
-            ////////////////////////////////
-            Ignore ignore = field.getAnnotation(Ignore.class);
-            if(ignore!=null){
-                continue;
-            }
-            PropertyName propertyNameAnnotation = field.getAnnotation(PropertyName.class);
-            String propertyName = field.getName();
-            if(propertyNameAnnotation!=null){
-                propertyName = propertyNameAnnotation.value();
-            }
-            Operation operation = Operation.EQUAL;
-            OperationType operationTypeAnnotation = field.getAnnotation(OperationType.class);
-            if(operationTypeAnnotation!=null){
-                operation = operationTypeAnnotation.value();
-            }
-            String conditionMethod = "";
-            ConditionMethod conditionMethodAnnotation = field.getAnnotation(ConditionMethod.class);
-            if(conditionMethodAnnotation!=null){
-                conditionMethod = conditionMethodAnnotation.value();
-            }
+            Field[] fields = obj.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                Object value = field.get(obj);
+                if (value == null) {
+                    continue;
+                }
+                if (!types.contains(value.getClass().getName()) && !(value instanceof List) && !(value instanceof Enum)) {
+                    continue;
+                }
+                ////////////////////////////////
+                Ignore ignore = field.getAnnotation(Ignore.class);
+                if (ignore != null) {
+                    continue;
+                }
+                PropertyName propertyNameAnnotation = field.getAnnotation(PropertyName.class);
+                String propertyName = field.getName();
+                if (propertyNameAnnotation != null) {
+                    propertyName = propertyNameAnnotation.value();
+                }
+                Operation operation = Operation.EQUAL;
+                OperationType operationTypeAnnotation = field.getAnnotation(OperationType.class);
+                if (operationTypeAnnotation != null) {
+                    operation = operationTypeAnnotation.value();
+                }
+                String conditionMethod = "";
+                ConditionMethod conditionMethodAnnotation = field.getAnnotation(ConditionMethod.class);
+                if (conditionMethodAnnotation != null) {
+                    conditionMethod = conditionMethodAnnotation.value();
+                }
 
-            MetaData metaData = new MetaData(propertyName,operation,conditionMethod,value,criteria,prefix,suffix,field.getType());
-            metaData.execute();
+                MetaData metaData = new MetaData(propertyName, operation, conditionMethod, value, criteria, prefix, suffix, field.getType());
+                metaData.execute();
+            }
+            return instance;
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException("query generator bug:" + e.getMessage());
         }
-        return instance;
     }
 }
